77638d1f13b695451dc15479b8193445c9684da0
Arne Hassel <arne.hassel@gmail.com>
2019-06-24 18:05:42 -0400

    Testing out mashlib as a separate app

--- src/index.js
+++ src/index.js
@@ -5,21 +5,21 @@ const UI = panes.UI
 
 window.$rdf = $rdf
 
-document.addEventListener('DOMContentLoaded', function () {
-  // Set up cross-site proxy
-  $rdf.Fetcher.crossSiteProxyTemplate = window.origin + '/xss/?uri={uri}'
-
-  // Authenticate the user
-  UI.authn.checkUser()
-    .then(function () {
-      // Set up the view for the current subject
-      const kb = UI.store
-      const uri = window.location.href
-      const subject = kb.sym(uri)
-      const outliner = panes.getOutliner(document)
-      outliner.GotoSubject(subject, true, undefined, true, undefined)
-    })
-})
+// document.addEventListener('DOMContentLoaded', function () {
+//   // Set up cross-site proxy
+//   $rdf.Fetcher.crossSiteProxyTemplate = window.origin + '/xss/?uri={uri}'
+//
+//   // Authenticate the user
+//   UI.authn.checkUser()
+//     .then(function () {
+//       // Set up the view for the current subject
+//       const kb = UI.store
+//       const uri = window.location.href
+//       const subject = kb.sym(uri)
+//       const outliner = panes.getOutliner(document)
+//       outliner.GotoSubject(subject, true, undefined, true, undefined)
+//     })
+// })
 
 window.onpopstate = function(event) {
   window.document.outline.GotoSubject($rdf.sym(window.document.location.href), true, undefined, true, undefined)
